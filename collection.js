4aea50e3de4a80dde5d78a824c60a9a2eea680e4
argj3nt <155830693+argj3nt@users.noreply.github.com>
2024-03-14 12:21:46 +0100

    Add files via upload

---
+++ collection.js
@@ -0,0 +1,103 @@
+const collection = [
+
+    {
+        mark: 'BMW',
+        model: 'M135i',
+        year: 2022,
+        picture: 'link/to/a/picture',
+        power: '306HP',
+        engine: '2.0L',
+        startprice: '58,320 €'
+      },
+
+    {
+        mark: 'BMW',
+        model: 'M2 Coupé',
+        year: 2022,
+        picture: 'link/to/a/picture',
+        power: '460HP',
+        engine: '3.0L',
+        startprice: '78,410 €'
+      },
+
+    {
+      mark: 'BMW',
+      model: 'M3 Competition',
+      year: 2022,
+      picture: m3c.png,
+      power: '480HP',
+      engine: '3.0L',
+      startprice: '91,135 €'
+    },
+
+
+      {
+        mark: 'BMW',
+        model: 'M4 Coupé',
+        year: 2022,
+        picture: 'link/to/a/picture',
+        power: '530HP',
+        engine: '3.0L',
+        startprice: '109,505 €'
+      },
+
+      {
+        mark: 'BMW',
+        model: 'M8 Grand Coupé',
+        year: 2022,
+        picture: 'link/to/a/picture',
+        power: '600HP',
+        engine: '4.4L',
+        startprice: '167,350 €'
+      },
+
+      {
+        mark: 'BMW',
+        model: 'X3 M ',
+        year: 2022,
+        picture: 'link/to/a/picture',
+        power: '480HP',
+        engine: '3.0L',
+        startprice: '102,150 €'
+      },
+
+      {
+        mark: 'BMW',
+        model: 'X4 M ',
+        year: 2022,
+        picture: 'link/to/a/picture',
+        power: '480HP',
+        engine: '3.0L',
+        startprice: '105,230 €'
+      },
+
+      {
+        mark: 'BMW',
+        model: 'X5 M ',
+        year: 2022,
+        picture: 'link/to/a/picture',
+        power: '625HP',
+        engine: '4.4L',
+        startprice: '157,550 €'
+      },
+
+      {
+        mark: 'BMW',
+        model: 'X6 M ',
+        year: 2022,
+        picture: 'link/to/a/picture',
+        power: '625HP',
+        engine: '4.4L',
+        startprice: '160,550  €'
+      },
+
+      {
+        mark: 'BMW',
+        model: 'X7 M ',
+        year: 2022,
+        picture: 'link/to/a/picture',
+        power: '530HP',
+        engine: '4.4L',
+        startprice: '134,410  €'
+      },
+  ]
